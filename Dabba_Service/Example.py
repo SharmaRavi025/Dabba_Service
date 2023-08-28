35770e3229550e576fe213ce7f11c89bccc67756
SharmaRavi025 <techravisharma025@gmail.com>
2023-08-29 01:15:44 +0530

    add files

---
+++ Dabba_Service/Example.py
@@ -0,0 +1,7 @@
+//Create a python program that takes an array of user objects as a prop. Each user object will have properties like `id`, `name`, `city`, and other relevant information. The component should render a list of users grouped by cities. Display the city name as a heading, and under each city, list the names of the users belonging to that city. 
+
+//For example, if the input List is: 
+
+const users = [ { id: 1, name: "John Doe", city: "New York" }, { id: 2, name: "Jane Smith", city: "Los Angeles" }, { id: 3, name: "Bob Johnson", city: "New York" }, { id: 4, name: "Alice Brown", city: "Chicago" }]; 
+
+print const user[city]
---
+++ app/models.py
@@ -0,0 +1,117 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+
+STATE_CHOICES = (
+    ('Andaman & Nicobar Islands','Andaman & Nicobar Islands'),
+    ('Andhara Pradesh','Andhara Pradesh'),
+    ('Arunachal Pradesh','Arunachal Pradesh'),
+    ('Assam','Assam'),
+    ('Bihar','Bihar'),
+    ('Chandigarh','Chandigarh'),
+    ('Chattisgarh','Chattisgarh'),
+    ('Dadra & Nagar Haveli','Dadra & Nagar Haveli'),
+    ('Daman and Diu','Daman and Diu'),
+    ('Delhi','Delhi'),
+    ('Goa','Goa'),
+    ('Gujrat','Gujrat'),
+    ('Haryana','Haryana'),
+    ('Himachal Pradesh','Himachal Pradesh'),
+    ('Jammu & Kashmir','Jammu & Kashmir'),
+    ('Jharkhand','Jharkhand'),
+    ('Karnataka','Karnataka'),
+    ('Kerala','Kerala'),
+    ('Lakshadweep','Lakshadweep'),
+    ('Madhya Pradesh','Madhya Pradesh'),
+    ('Maharashtra','Maharashtra'),
+    ('Manipur','Manipur'),
+    ('Meghalaya','Meghalaya'),
+    ('Mizoram','Mizoram'),
+    ('Nagaland','Nagaland'),
+    ('Odisa','Odisa'),
+    ('Puducherry','Puducherry'),
+    ('Punjab','Punjab'),
+    ('Rajasthan','Rajasthan'),
+    ('Sikkim','Sikkim'),
+    ('Tamil Nadu','Tamil Nadu'),
+    ('Telangana','Telangana'),
+    ('Tripura','Tripura'),
+    ('Uttarakhand','Uttarakhand'),
+    ('Uttar Pradesh','Uttar Pradesh'),
+    ('West Bangal','West Bangal'),
+     
+)
+
+CATEGORY_CHOICES=(
+    ('TF','Traditional_Food'),
+    ('SF','South_Indian_Food'),
+    ('HK','Home_Kitchen'),
+    ('FF','Famous_Food'),
+    ('MK','Mummy_Kitchen'),
+    ('HW','Health_is_Wealth'),
+)
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    selling_price = models.FloatField()
+    discounted_price =models.FloatField()
+    description = models.TextField()
+    composition = models.TextField(default='')
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+    product_image = models.ImageField(upload_to='product')
+    def __str__(self):
+        return self.title
+    
+class Customer(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    mobile = models.CharField(default='',max_length=10)
+    locality = models.CharField(max_length=200)
+    city = models.CharField(max_length=50)
+    state = models.CharField(choices=STATE_CHOICES,max_length=100)
+    zipcode = models.CharField(max_length=6)
+    def __str__(self):
+        return self.name
+    
+class Cart(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    @property
+    def total_cost(self):
+        return self.quantity * self.product.discounted_price
+ 
+STATUS_CHOICES = (
+    ('Accepted','Accepted'),
+    ('Packed','Packed'),
+    ('On The Way','On The Way'),
+    ('Delivered','Delivered'),
+    ('Cancel','Cancel'),
+    ('Pending','Pending'),
+)
+
+class Payment(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    amount = models.FloatField()
+    razorpay_order_id = models.CharField(max_length=100,blank=True,null=True)
+    razorpay_payment_status = models.CharField(max_length=100,blank=True,null=True)
+    razorpay_payment_id = models.CharField(max_length=100,blank=True,null=True)
+    paid = models.BooleanField(default=False)
+
+class OrderPlaced(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer,on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    quantity =models.PositiveIntegerField(default=1)
+    ordered_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=50,choices=STATUS_CHOICES,default='Pending')
+    payment = models.ForeignKey(Payment,on_delete=models.CASCADE,default="")
+    @property
+    def total_cost(self):
+        return self.quantity * self.product.discounted_price
+    
+class Wishlist(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
